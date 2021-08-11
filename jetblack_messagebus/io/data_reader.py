506a9a7d794a9100a51ea0aa980c2a322a4cefb3
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-08-11 07:03:48 +0100

    Tidy up

--- jetblack_messagebus/io/data_reader.py
+++ jetblack_messagebus/io/data_reader.py
@@ -9,34 +9,57 @@ from .data_packet import DataPacket
 
 
 class DataReader:
-    """A portable data reader class"""
+    """A data reader class"""
 
     def __init__(self, reader: StreamReader) -> None:
         self.reader = reader
 
     async def read_boolean(self) -> bool:
-        """Read a boolean"""
+        """Read a boolean.
+
+        Returns:
+            bool: The boolean.
+        """
         buf = await self.reader.readexactly(1)
         return struct.unpack('?', buf)[0]
 
     async def read_byte(self) -> int:
-        """RRead a byte"""
+        """Read a byte.
+
+        Returns:
+            int: The byte.
+        """
         buf = await self.reader.readexactly(1)
         return struct.unpack('b', buf)[0]
 
     async def read_int(self) -> int:
-        """Read a 4 byte int"""
+        """Read an int.
+
+        Returns:
+            int: The int.
+        """
         buf = await self.reader.readexactly(4)
         return struct.unpack('>i', buf)[0]
 
     async def read_string(self, encoding: str = 'utf-8') -> str:
-        """Read a string"""
+        """Read a string.
+
+        Args:
+            encoding (str, optional): The encoding. Defaults to 'utf-8'.
+
+        Returns:
+            str: The string.
+        """
         count = await self.read_int()
         buf = await self.reader.readexactly(count)
         return buf.decode(encoding)
 
     async def read_byte_array(self) -> Optional[bytes]:
-        """Read a byte array"""
+        """Read an array of bytes.
+
+        Returns:
+            Optional[bytes]: The bytes or None.
+        """
         count = await self.read_int()
         if count == 0:
             return None
@@ -44,12 +67,20 @@ class DataReader:
         return buf
 
     async def read_uuid(self) -> UUID:
-        """Read a UUID"""
+        """Read a UUID.
+
+        Returns:
+            UUID: The UUID.
+        """
         buf = await self.reader.readexactly(16)
         return UUID(bytes_le=buf)
 
     async def read_int_set(self) -> Optional[Set[int]]:
-        """Read a set of ints"""
+        """Read a set of ints
+
+        Returns:
+            Optional[Set[int]]: The set of ints or None.
+        """
         count = await self.read_int()
         if count == 0:
             return None
@@ -60,16 +91,20 @@ class DataReader:
         return data
 
     async def read_data_packet(self) -> DataPacket:
-        """Read a data packet"""
+        """Read a data packet
+
+        Returns:
+            DataPacket: The data packet.
+        """
         entitlements = await self.read_int_set()
         data = await self.read_byte_array()
         return DataPacket(entitlements, data)
 
     async def read_data_packet_array(self) -> Optional[List[DataPacket]]:
-        """Read a list of data packets
+        """Read an array of data packets.
 
-        :return: A list of data packets
-        :rtype: Optional[List[DataPacket]]
+        Returns:
+            Optional[List[DataPacket]]: The data packets or None.
         """
         count = await self.read_int()
         if count == 0:
--- jetblack_messagebus/io/data_writer.py
+++ jetblack_messagebus/io/data_writer.py
@@ -15,26 +15,38 @@ class DataWriter:
         self.writer = writer
 
     def write_boolean(self, val: bool) -> None:
-        """Write a boolean"""
+        """Write a boolean
+
+        Args:
+            val (bool): Th boolean value.
+        """
         buf = struct.pack('?', val)
         self.writer.write(buf)
 
     def write_byte(self, val: int) -> None:
-        """Write a byte"""
+        """Write a byte
+
+        Args:
+            val (int): The value to write.
+        """
         buf = struct.pack('b', val)
         self.writer.write(buf)
 
     def write_int(self, val) -> None:
-        """Write a 4 byte int"""
+        """Write an int
+
+        Args:
+            val ([type]): The int value.
+        """
         self.writer.write(struct.pack('>i', val))
 
-    def write_char(self, val: str) -> None:
-        """Write a 2 byte char"""
-        buf = struct.pack('>H', ord(val[0]))
-        self.writer.write(buf)
-
     def write_string(self, val: Optional[str], encoding: str = 'utf-8') -> None:
-        """Write a string"""
+        """Writ a string.
+
+        Args:
+            val (Optional[str]): The string to write.
+            encoding (str, optional): The encoding. Defaults to 'utf-8'.
+        """
         if val is None:
             self.write_int(0)
         else:
@@ -43,7 +55,11 @@ class DataWriter:
             self.writer.write(buf)
 
     def write_byte_array(self, val: Optional[bytes]) -> None:
-        """Write a byte array"""
+        """Write an array of bytes.
+
+        Args:
+            val (Optional[bytes]): The bytes to write.
+        """
         if val is None:
             self.write_int(0)
         else:
@@ -51,11 +67,19 @@ class DataWriter:
             self.writer.write(val)
 
     def write_uuid(self, val: UUID) -> None:
-        """Write a UUID"""
+        """Write a UUID
+
+        Args:
+            val (UUID): The id to write.
+        """
         self.writer.write(val.bytes_le)
 
     def write_int_set(self, val: Optional[Set[int]]) -> None:
-        """Write a set of integers"""
+        """Writ a set of ints
+
+        Args:
+            val (Optional[Set[int]]): The set or None.
+        """
         if val is None:
             self.write_int(0)
         else:
@@ -64,15 +88,19 @@ class DataWriter:
                 self.write_int(item)
 
     def write_data_packet(self, val: DataPacket) -> None:
-        """Write a data packet"""
+        """Write a data packet.
+
+        Args:
+            val (DataPacket): The data packets.
+        """
         self.write_int_set(val.entitlements)
         self.write_byte_array(val.data)
 
     def write_data_packet_array(self, val: Optional[List[DataPacket]]) -> None:
-        """Write a list of data packets
+        """Write an array of data packets.
 
-        :param val: The list of data packets
-        :type val: Optional[List[DataPacket]]
+        Args:
+            val (Optional[List[DataPacket]]): The data packets or None.
         """
         if val is None:
             self.write_int(0)
@@ -82,11 +110,13 @@ class DataWriter:
                 self.write_data_packet(packet)
 
     async def drain(self) -> None:
-        """Drain the writer"""
+        """Drain the writer.
+        """
         await self.writer.drain()
 
     async def close(self) -> None:
-        """Close the connection"""
+        """Close the connection
+        """
         if self.writer.can_write_eof():
             self.writer.write_eof()
             await self.writer.drain()
